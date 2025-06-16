e698665955a8d76d15fb2e764322c0ab38c2c960
Kundisivashankar <kundisivashankar95@gmail.com>
2025-06-17 00:26:53 +0530

    Initial commit

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Chinnu',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Blessy',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Bunny',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Ishanth',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Shobitha',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Katta Man',
+    avatar: 'user-6.jpg',
+  },
+];
